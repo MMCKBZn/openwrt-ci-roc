7f35213c6996d70a0c578cd421d3268f93ab819c
MMCKBZn <1979161564@qq.com>
2026-05-01 16:02:21 +0800

    Update Roc-script.sh

--- scripts/Roc-script.sh
+++ scripts/Roc-script.sh
@@ -5,25 +5,20 @@ sed -i "s/hostname='.*'/hostname='Roc'/g" package/base-files/files/bin/config_ge
 FILE="feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js"
 TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
 
-perl -i -pe "
-if (/_('Firmware Version'),/) {
-    s|_('Firmware Version'),\s*\(L\.isObject\(boardinfo\.release\)\s*\?\s*boardinfo\.release\.description\s*\+\s*'\s*\/\s*'\s*:\s*''\)\s*\+\s*\(\s*luciversion\s*\|\|\s*''\s*\)|
-        _('Firmware Version'),
-        E('span', {}, [
-            (L.isObject(boardinfo.release)
-                ? boardinfo.release.description + ' / '
-                : '') + (luciversion || '') + ' / ',
-            E('a', {
-                href: 'https://github.com/laipeng668/openwrt-ci-roc/releases',
-                target: '_blank',
-                rel: 'noopener noreferrer'
-            }, [ '原作者 by Roc' ]),
-            E('span', {}, [ ' & ' ]),
-            E('a', {
-                href: 'https://github.com/MMCKBZn/openwrt-ci-roc/releases/tag/JDCloud',
-                target: '_blank',
-                rel: 'noopener noreferrer'
-            }, [ '修改者 by MMCKB ${TIMESTAMP}' ])
-        ]),|
+cat > /tmp/firmware_replace.txt << EOF
+            _('Firmware Version'),
+            E('span', {}, [
+                (L.isObject(boardinfo.release) ? boardinfo.release.description + ' / ' : '') + (luciversion || '') + ' / ',
+                E('a', { href: 'https://github.com/laipeng668/openwrt-ci-roc/releases', target: '_blank', rel: 'noopener noreferrer' }, [ '原作者 by Roc' ]),
+                E('span', {}, [ ' & ' ]),
+                E('a', { href: 'https://github.com/MMCKBZn/openwrt-ci-roc/releases/tag/JDCloud', target: '_blank', rel: 'noopener noreferrer' }, [ '修改者 by MMCKB ${TIMESTAMP}' ])
+            ]),
+EOF
+
+sed -i "/_('Firmware Version'),\s*(L\.isObject(boardinfo\.release)/{
+    r /tmp/firmware_replace.txt
+    d
 }" "$FILE"
-# ----------------------------------------------------------------------
+
+rm -f /tmp/firmware_replace.txt
+# -------------------------------------------------------------
